8664ec45372978db6701c3842a024b17a2d601d2
Efren511 <luisefrengarciadeleon@gmail.com>
2022-09-17 17:00:21 -0500

    The first version of the game

---
+++ main.py
@@ -0,0 +1,13 @@
+Banner = """
+
+"""
+
+# We make the main funtion
+def main():
+    # We show the banner
+    print(Banner)
+
+# We make a access point
+if __name__ == '__main__':
+    # We call the main funtion
+    main()
